337606327a12814b739f863f850e5e47bcd98c2b
Iram Mendoza <123773810+IramAnt@users.noreply.github.com>
2023-02-16 00:48:04 -0600

    Ejercicios javascript

---
+++ Codigo.js
@@ -0,0 +1,12 @@
+var cajon1 = 95;
+var cajon2 = 4;
+var cajon3 = cajon1 % cajon2;
+console.log (cajon3)
+var saludo = "Hola";
+var saludo_completo = saludo + " Letys";
+console.log (saludo_completo);
+// Asi se escribe sin alterar el codigo
+function sumas (x)
+{return x + 5;}
+var suma = sumas (3)
+console.log(suma)
